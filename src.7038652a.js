b0759b6fafc90fe3ab2018881761ec5a9283a220
SovaV <77897131+SovaV@users.noreply.github.com>
2021-04-20 07:04:06 +0000

    Deploying to gh-pages from @ SovaV/test@501bedaf0eb9f4ea8e63fa905c1978e51def7cf2 🚀

--- src.7038652a.js
+++ src.7038652a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test/src.27484b5a.js.map
+//# sourceMappingURL=/test/src.7038652a.js.map
